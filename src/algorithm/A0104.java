3c7ab808196d6d63a5494feda082229d7e419402
h0l1da2 <whaleee@naver.com>
2024-03-12 23:58:40 +0900

    feat: 01-04 단어 뒤집기
    1. 몇 개의 단어를 받아오는지 값을 받아오고, 그 길이대로 문자열 배열을 생성
    2. 문자열 배열 길이대로 반복해서 문자를 입력받음
    3. 결과물을 담을 result 문자열 선언
    4. 입력 받은 문자열 배열대로 반복문을 돌림.
    
    5. 일단 문자열을 하나씩 자름.
    6. 자른 문자열 길이도 받음.
    - 배열 마지막 문자부터 추가해야 문자열을 뒤집을 수 있기 때문에 길이 - 1 을 해줌, 배열 마지막 인덱스로 사용하기 위함임.
    7. 배열 길이대로 반복문을 돌림.
    - 빈 문자열에 배열 마지막부터 저장함(뒤집기 위함)
    8. 반복문이 끝났다면 문자열이 뒤집어서 저장됐을테니, 출력해줌.
    9. 다음 문자열 출력을 위해 결과 문자열을 담을 변수는 초기화 !
    
    master

---
+++ src/algorithm/A0104.java
@@ -0,0 +1,50 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class A0104 {
+
+    /**
+     * 설명
+     * N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+     * <p>
+     * 입력
+     * 첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+     * 두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+     * <p>
+     * 출력
+     * N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+     * <p>
+     * 예시 입력
+     * 3
+     * good
+     * Time
+     * Big
+     * 예시 출력
+     * doog
+     * emiT
+     * giB
+     */
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+        String[] strArr = new String[count];
+        for (int i = 0; i < count; i++) {
+            String inputStr = in.next();
+            strArr[i] = inputStr;
+        }
+
+        String result = "";
+        for (String str : strArr) {
+            String[] splitStr = str.split("");
+            int arrayIndex = str.length() - 1;
+            for (int i = 0; i < splitStr.length; i++) {
+                result += splitStr[arrayIndex--];
+            }
+            System.out.println(result);
+            result = "";
+        }
+    }
+}
