1ddcabc50553acd1259eff372b10dfe26a4aa815
h0l1da2 <whaleee@naver.com>
2024-07-11 21:49:55 +0900

    feat: 05-08 응급실
    1. Person 객체를 만들어준다.
    - 위험도가 같아도 그 중에 몇 번째로 줄을 섰었는지에 따라 순서가 달라질 수 있음.
    - 그래서 id 와 number(위험도)를 가진 객체를 만들어서, 그 객체를 넣는다.
    2. que 에 Person 객체로 입력받은 환자를 넣어주면서 list 에 위험도를 따로 넣어준다.
    3. 넣어준 위험도 리스트는 역순으로 정렬해준다.
    이제 while 문으로 위험 환자를 찾아준다.
    4. pointer 는 제일 위험한 환자의 위험도이다. (역순으로 정렬해놔서 가능) index 로 사람 하나 빠져나갈때마다 인덱스도 +1 해주면 됨.
    5. person 하나씩 꺼내서 최대 위험도인지 대조해준다.
    - 최대위험도이면 일단 한번 돌았으니 result++ 해주고
    - id 까지 확인해서 입력받은 m번째 사람이면 break; 해주고 몇번 돌았는지 리턴하면 됨
    6. 최대위험도 아니라면 que에 다시 넣어주면 됨

---
+++ src/algorithm/e/E0508.java
@@ -0,0 +1,84 @@
+package algorithm.e;
+
+import java.util.*;
+
+public class E0508 {
+
+    /**
+     * 응급실
+     * 설명
+     * 메디컬 병원 응급실에는 의사가 한 명밖에 없습니다.
+     * 응급실은 환자가 도착한 순서대로 진료를 합니다. 하지만 위험도가 높은 환자는 빨리 응급조치를 의사가 해야 합니다.
+     * 이런 문제를 보완하기 위해 응급실은 다음과 같은 방법으로 환자의 진료순서를 정합니다.
+     * • 환자가 접수한 순서대로의 목록에서 제일 앞에 있는 환자목록을 꺼냅니다.
+     * • 나머지 대기 목록에서 꺼낸 환자 보다 위험도가 높은 환자가 존재하면 대기목록 제일 뒤로 다시 넣습니다. 그렇지 않으면 진료를 받습니다.
+     * 즉 대기목록에 자기 보다 위험도가 높은 환자가 없을 때 자신이 진료를 받는 구조입니다.
+     * 현재 N명의 환자가 대기목록에 있습니다.
+     * N명의 대기목록 순서의 환자 위험도가 주어지면, 대기목록상의 M번째 환자는 몇 번째로 진료를 받는지 출력하는 프로그램을 작성하세요.
+     * 대기목록상의 M번째는 대기목록의 제일 처음 환자를 0번째로 간주하여 표현한 것입니다.
+     * <p>
+     * 입력
+     * 첫 줄에 자연수 N(5<=N<=100)과 M(0<=M<N) 주어집니다.
+     * 두 번째 줄에 접수한 순서대로 환자의 위험도(50<=위험도<=100)가 주어집니다.
+     * 위험도는 값이 높을 수록 더 위험하다는 뜻입니다. 같은 값의 위험도가 존재할 수 있습니다.
+     * <p>
+     * 출력
+     * M번째 환자의 몇 번째로 진료받는지 출력하세요.
+     * <p>
+     * 예시 입력
+     * 5 2
+     * 60 50 70 80 90
+     * 예시 출력
+     * 3
+     */
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        Queue<Person> que = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i=0; i<n; i++) {
+            int tmp = in.nextInt();
+
+            que.offer(new Person(i, tmp));
+            list.add(tmp);
+        }
+
+        // 높은 순서대로 정렬 !
+        list.sort(Collections.reverseOrder());
+
+        int result = 0;
+
+        int index = 0;
+        while (true) {
+            int pointer = list.get(index);
+            Person person = que.poll();
+            // 현재 최대 위험도를 가진 인간
+            if (pointer == person.number) {
+                // 해당자인지 검사하면 break; 하고 result 리턴, 아니라면?
+                result++;
+                if (person.id == m) {
+                    break;
+                }
+                index++;
+            } else {
+                que.offer(person);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
+
+class Person {
+    int id;
+    int number;
+
+    public Person(int id, int number) {
+        this.id = id;
+        this.number = number;
+    }
+}
